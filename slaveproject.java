aabcc454ad4caa26574c9afb2bd96bd44c34d7e1
SagarDimbar <sagardimbar1234@gmail.com>
2024-01-19 09:24:08 +0530

    new file added

---
+++ slaveproject.java
@@ -0,0 +1,24 @@
+package project1;
+
+class Example {
+	
+     void fun1() {
+		
+		System.out.println("C");
+	}
+	
+	static void fun2() {
+		
+		System.out.println("A"); 	
+	}
+	
+	public static void main(String []args) {
+		System.out.println("B");
+		fun2();
+	}
+
+}
+
+
+
+
